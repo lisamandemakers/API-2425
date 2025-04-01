b21e6a2ee02647cc0148b23f11fba1ca575f56b5
Lisa <lisamandemakers@hotmail.com>
2025-04-01 14:58:20 +0200

    server working

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+// 1. Imports, laad externe modules (packages) in.
 import 'dotenv/config';
 import { App } from '@tinyhttp/app';
 import { logger } from '@tinyhttp/logger';
 import { Liquid } from 'liquidjs';
 import sirv from 'sirv';
 
+// 2. Data-object, dit is nu de content van de website en gaan we later dynamisch maken
 const data = {
   'beemdkroon': {
     id: 'beemdkroon',
@@ -27,21 +29,27 @@ const data = {
   }
 }
 
+// 3. LiquidJS instellen, zorgt ervoor dat alle .liquid bestanden worden verwerkt
 const engine = new Liquid({
   extname: '.liquid',
 });
 
+// 4. Webserver maken met TinyHTTP
 const app = new App();
-
+// 5. Middelware instellen en service starten
+// Logger() laat in de terminal zien welke pagina's worden geladen.
 app
   .use(logger())
   .use('/', sirv('dist'))
   .listen(3000, () => console.log('Server available on http://localhost:3000'));
 
+// 6. Route voort home pagina
+// Render template zorgt ervoor dat de home page word gelinkt met de index.liquid & lijst van bloemen
 app.get('/', async (req, res) => {
   return res.send(renderTemplate('server/views/index.liquid', { title: 'Home', items: Object.values(data) }));
 });
 
+// 7. Route voor een detailpagina
 app.get('/plant/:id/', async (req, res) => {
   const id = req.params.id;
   const item = data[id];
@@ -51,6 +59,9 @@ app.get('/plant/:id/', async (req, res) => {
   return res.send(renderTemplate('server/views/detail.liquid', { title: `Detail page for ${id}`, item }));
 });
 
+// 8. Functie om liquid templates te renderen
+// .liquid docs worden gevuld → kijkt of de app in ontwikkelmodus (dev) of live (production) staat
+// engine.renderFileSync(template, templateData) → Rendert het opgegeven Liquid-template en vult het met data.
 const renderTemplate = (template, data) => {
   const templateData = {
     NODE_ENV: process.env.NODE_ENV || 'production',
